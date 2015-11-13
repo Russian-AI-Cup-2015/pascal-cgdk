a18f46c4201c0631ff17f2c39c7affab2690dc3d
Tumaso <igor.kuznetsov@tumaso.com>
2015-11-13 17:45:40 +0500

    Update CircularUnitControl.pas
    Add properties support;
    Apply pascal code style.

--- CircularUnitControl.pas
+++ CircularUnitControl.pas
@@ -3,44 +3,41 @@ unit CircularUnitControl;
 interface
 
 uses
-    Math, TypeControl, UnitControl;
+  Math, TypeControl, UnitControl;
 
 type
-    TCircularUnit = class (TUnit)
-    private
-        FRadius: Double;
+  TCircularUnit = class(TUnit)
+  private
+    FRadius: Double;
+    function GetRadius: Double;
+  public
+    property Radius: Double read GetRadius;
+    constructor Create(const AId: Int64; const AMass: Double; const AX: Double; const AY: Double; 
+      const ASpeedX: Double; const ASpeedY: Double; const AAngle: Double; const AAngularSpeed: Double; 
+      const ARadius: Double);
+    destructor Destroy; override;
+  end;
 
-    protected
-        constructor Create(id: Int64; mass: Double; x: Double; y: Double; speedX: Double; speedY: Double; angle: Double;
-                angularSpeed: Double; radius: Double);
-
-    public
-        function GetRadius: Double;
-
-        destructor Destroy; override;
-
-    end;
-
-    TCircularUnitArray = array of TCircularUnit;
+  TCircularUnitArray = array of TCircularUnit;
 
 implementation
 
-constructor TCircularUnit.Create(id: Int64; mass: Double; x: Double; y: Double; speedX: Double; speedY: Double;
-        angle: Double; angularSpeed: Double; radius: Double);
-begin
-    inherited Create(id, mass, x, y, speedX, speedY, angle, angularSpeed);
-
-    FRadius := radius;
-end;
-
 function TCircularUnit.GetRadius: Double;
 begin
-    result := FRadius;
+  Result := FRadius;
+end;
+
+constructor TCircularUnit.Create(const AId: Int64; const AMass: Double; const AX: Double; const AY: Double; 
+  const ASpeedX: Double; const ASpeedY: Double; const AAngle: Double; const AAngularSpeed: Double; 
+  const ARadius: Double);
+begin
+  inherited Create(AId, AMass, AX, AY, ASpeedX, ASpeedY, AAngle, AAngularSpeed);
+  FRadius := ARadius;
 end;
 
 destructor TCircularUnit.Destroy;
 begin
-    inherited;
+  inherited;
 end;
 
 end.
