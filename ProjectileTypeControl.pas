6146e7ce42dd206f79e915693f830e068172d566
Tumaso <igor.kuznetsov@tumaso.com>
2015-11-13 17:51:54 +0500

    Update ProjectileTypeControl.pas
    Apply pascal code style.

--- ProjectileTypeControl.pas
+++ ProjectileTypeControl.pas
@@ -3,21 +3,21 @@ unit ProjectileTypeControl;
 interface
 
 uses
-    TypeControl;
+  TypeControl;
 
 const
-    _UNKNOWN_PROJECTILE_TYPE_: LongInt = -1;
-    WASHER                   : LongInt = 0;
-    TIRE                     : LongInt = 1;
-    _PROJECTILE_TYPE_COUNT_  : LongInt = 2;
+  _UNKNOWN_PROJECTILE_TYPE_: LongInt = -1;
+  WASHER                   : LongInt = 0;
+  TIRE                     : LongInt = 1;
+  _PROJECTILE_TYPE_COUNT_  : LongInt = 2;
 
 type
-    TProjectileType = LongInt;
-    TProjectileTypeArray = TLongIntArray;
-    TProjectileTypeArray2D = TLongIntArray2D;
-    TProjectileTypeArray3D = TLongIntArray3D;
-    TProjectileTypeArray4D = TLongIntArray4D;
-    TProjectileTypeArray5D = TLongIntArray5D;
+  TProjectileType = LongInt;
+  TProjectileTypeArray = TLongIntArray;
+  TProjectileTypeArray2D = TLongIntArray2D;
+  TProjectileTypeArray3D = TLongIntArray3D;
+  TProjectileTypeArray4D = TLongIntArray4D;
+  TProjectileTypeArray5D = TLongIntArray5D;
 
 implementation
 
