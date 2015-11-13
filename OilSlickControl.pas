7fd3010917438c0163ad14de097806cb71f02569
Tumaso <igor.kuznetsov@tumaso.com>
2015-11-13 17:49:34 +0500

    Update OilSlickControl.pas
    Add properties support;
    Apply pascal code style.

--- OilSlickControl.pas
+++ OilSlickControl.pas
@@ -3,43 +3,41 @@ unit OilSlickControl;
 interface
 
 uses
-    Math, TypeControl, CircularUnitControl;
+  Math, TypeControl, CircularUnitControl;
 
 type
-    TOilSlick = class (TCircularUnit)
-    private
-        FRemainingLifetime: LongInt;
+  TOilSlick = class(TCircularUnit)
+  private
+    FRemainingLifetime: LongInt;
+    function GetRemainingLifetime: LongInt;
+  public
+    property RemainingLifetime: LongInt read GetRemainingLifetime; 
+    constructor Create(const AId: Int64; const AMass: Double; const AX: Double; const AY: Double; 
+      const ASpeedX: Double; const ASpeedY: Double; const AAngle: Double; const AAngularSpeed: Double; 
+      const ARadius: Double; const ARemainingLifetime: LongInt);
+    destructor Destroy; override;
+  end;
 
-    public
-        constructor Create(id: Int64; mass: Double; x: Double; y: Double; speedX: Double; speedY: Double; angle: Double;
-                angularSpeed: Double; radius: Double; remainingLifetime: LongInt);
-
-        function GetRemainingLifetime: LongInt;
-
-        destructor Destroy; override;
-
-    end;
-
-    TOilSlickArray = array of TOilSlick;
+  TOilSlickArray = array of TOilSlick;
 
 implementation
 
-constructor TOilSlick.Create(id: Int64; mass: Double; x: Double; y: Double; speedX: Double; speedY: Double;
-        angle: Double; angularSpeed: Double; radius: Double; remainingLifetime: LongInt);
-begin
-    inherited Create(id, mass, x, y, speedX, speedY, angle, angularSpeed, radius);
-
-    FRemainingLifetime := remainingLifetime;
-end;
-
 function TOilSlick.GetRemainingLifetime: LongInt;
 begin
-    result := FRemainingLifetime;
+  Result := FRemainingLifetime;
+end;
+
+constructor TOilSlick.Create(const AId: Int64; const AMass: Double; const AX: Double; const AY: Double; 
+  const ASpeedX: Double; const ASpeedY: Double; const AAngle: Double; const AAngularSpeed: Double; 
+  const ARadius: Double; const ARemainingLifetime: LongInt);
+begin
+  inherited Create(AId, AMass, AX, AY, ASpeedX, ASpeedY, AAngle, AAngularSpeed, ARadius);
+  FRemainingLifetime := ARemainingLifetime;
 end;
 
 destructor TOilSlick.Destroy;
 begin
-    inherited;
+  inherited;
 end;
 
 end.
