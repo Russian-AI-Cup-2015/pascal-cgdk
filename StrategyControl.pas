4792481181889320c11784f4f9a62b2e02c9a199
SladeThe <sladethe@gmail.com>
2015-12-23 19:27:40 +0300

    Accepted some proposals made by peroff: extended list of ignored files; supported UNICODE IDEs and compilers; added virtual TStrategy.Create; suppressed FPC 'Constructor should be public' warning.

--- StrategyControl.pas
+++ StrategyControl.pas
@@ -8,10 +8,16 @@ uses
 type
   TStrategy = class
   public
+    constructor Create; virtual;
     procedure Move(me: TCar; world: TWorld; game: TGame; move: TMove); virtual; abstract;
 
   end;
 
 implementation
+
+constructor TStrategy.Create;
+begin
+  inherited Create;
+end;
 
 end.
--- compile-fpc.bat
+++ compile-fpc.bat
@@ -7,7 +7,7 @@ if not exist %name%.pas (
 
 del /F /Q %name%.exe
 
-fpc.exe -dONLINE_JUDGE -Cs67107839 -Mdelphi -XS Runner.dpr -o%name%.exe 1>compilation.log
+fpc.exe -dONLINE_JUDGE -Cs67107839 -Mdelphi -XS -vm3018 Runner.dpr -o%name%.exe 1>compilation.log
 
 if exist Runner.exe (
     move /Y Runner.exe %name%.exe
